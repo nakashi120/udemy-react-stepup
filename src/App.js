295ff62210536751be33fd3d4e4743510908cd53
nakashi120 <nakashi120@gmail.com>
2021-03-07 16:39:14 +0900

    styled jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { InlineStyle } from "./components/InlineStyle"
 import { CssModules } from "./components/CssModules"
+import { StyledJsx } from "./components/StyledJsx"
 
 function App() {
   return (
     <>
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </>
   )
 }
