e107f267f23bd48276bc799a1c1495d933ccf6eb
nakashi120 <nakashi120@gmail.com>
2021-03-07 16:31:53 +0900

    css module

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import React, { useState, useCallback, useMemo } from "react"
-import { ChildArea } from "./ChildArea"
+import React from "react"
+import { InlineStyle } from "./components/InlineStyle"
+import { CssModules } from "./components/CssModules"
 
 function App() {
-  console.log("App")
-  const [text, setText] = useState("")
-  const [open, setOpen] = useState(false)
-
-  const onChangeText = (e) => {
-    setText(e.target.value)
-  }
-
-  const onClickOpen = () => {
-    setOpen(!open)
-  }
-
-  const onClickClose = useCallback(() => setOpen(false), [setOpen])
-
-  const temp = useMemo(() => 1 + 3, [])
-  console.log(temp)
-
   return (
     <>
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+      <InlineStyle />
+      <CssModules />
     </>
   )
 }
