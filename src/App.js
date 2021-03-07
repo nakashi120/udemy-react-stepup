71fdb139a65c20734788100cf4e8220620edf943
nakashi120 <nakashi120@gmail.com>
2021-03-07 20:10:32 +0900

    react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom"
+
 import React from "react"
-import { InlineStyle } from "./components/InlineStyle"
-import { CssModules } from "./components/CssModules"
-import { StyledJsx } from "./components/StyledJsx"
-import { StyledComponents } from "./components/StyledComponents"
-import { Emotion } from "./components/Emotion"
+import { Home } from "./Home"
+import { Page1 } from "./Page1"
+import { Page2 } from "./Page2"
 
 function App() {
   return (
-    <>
-      <InlineStyle />
-      <CssModules />
-      <StyledJsx />
-      <StyledComponents />
-      <Emotion />
-    </>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
