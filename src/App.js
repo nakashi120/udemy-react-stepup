b1511c9e3c1aaf03bced849572d442a657367f1b
nakashi120 <nakashi120@gmail.com>
2021-03-07 15:43:55 +0900

    use memo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { ChildArea } from "./ChildArea"
 
 function App() {
+  console.log("App")
+  const [text, setText] = useState("")
+  const [open, setOpen] = useState(false)
+
+  const onChangeText = (e) => {
+    setText(e.target.value)
+  }
+
+  const onClickOpen = () => {
+    setOpen(!open)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+    </>
+  )
 }
 
-export default App;
+export default App
